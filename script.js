3441b39b944fb0391c71f956387c38b9201457ec
ThomasAlberto21 <thomasalberto456@gmail.com>
2022-08-23 19:40:17 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// gambar
+function toggleElement() {
+    var x = document.getElementById("gambar");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
